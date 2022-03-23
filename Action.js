b31a69e5955ff63c6d698d5635f2d601476869ab
Ashish Shukla <100287606+Ashishshukla776@users.noreply.github.com>
2022-03-23 20:32:14 +0530

    Action.js

--- Action.js
+++ Action.js
@@ -1,4 +1,4 @@
-export const  SET_USRE_NAME='SET_USER_NAME';
+export const  SET_USER_NAME='SET_USER_NAME';
 export const  SET_USER_PASSWORD='SET_USER_PASSWORD';
 
 export const setName=name=>dispatch=>{
