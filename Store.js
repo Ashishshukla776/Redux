2d79cf1cae45bdd7b530e3d764a2740af3faeb0a
Ashish Shukla <100287606+Ashishshukla776@users.noreply.github.com>
2022-03-28 22:19:48 +0530

    Delete Store.js

--- Store.js
+++
@@ -1,7 +0,0 @@
-import {createStore,combineReducers,applyMiddleware} from 'redux';
-import thunk from '@reduxjs/toolkit';
-import {reducer} from './Reducer';
-
-const rootReducer=combineReducers({reducer});
-export const store=createStore(
-    rootReducer,applyMiddleware(thunk))
